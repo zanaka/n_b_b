d761788b7d569a60fda9504e154f94cee958527d
e155745 <e155745@ie.u-ryukyu.ac.jp>
2017-09-26 15:24:17 +0900

    create post

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,23 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all.order(created_at: :desc)
+  end
+  
+  def show
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def new
+	  @post = Post.new
+  end
+
+  def create
+  	    @post = Post.new(content: params[:content])
+    if @post.save
+      flash[:notice] = "投稿を作成しました"
+      redirect_to("/posts/index")
+    else
+      render("posts/new")
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/new' => "posts#new"
+  post 'posts/create' => "posts#create"
   get 'home/top'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
